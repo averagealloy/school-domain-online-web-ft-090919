92c59352df99b80947981e3f76f371ba8a94dfd6
Mike McTighe <m.mctighe10@gmail.com>
2019-09-18 15:24:36 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -4,12 +4,12 @@ attr_reader :School, :roster
 def initialize(name)
 @school = name
 @roster = {}
-end 
+end
 def add_student(name, grade)
   if !@roster.has_key?(grade)
     @roster[grade] = []
   end
   @roster[grade] << name
-    end
+  
   end
 end
